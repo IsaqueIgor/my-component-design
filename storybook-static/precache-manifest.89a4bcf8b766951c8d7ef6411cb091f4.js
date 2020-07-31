6d3b87bcfdcd50b3509497f7d690c50b312fc4e2
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-07-31 11:24:44 -0300

    feat: add Storybook-statick

---
+++ storybook-static/precache-manifest.89a4bcf8b766951c8d7ef6411cb091f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c93517fa9e80f4c7c472a9ee6b377685",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.035b21baaa543542001c.bundle.js"
+  },
+  {
+    "url": "runtime~main.035b21baaa543542001c.bundle.js"
+  },
+  {
+    "revision": "77b4ee166ccdcf8134673d7a35822098",
+    "url": "static/media/SignIn.77b4ee16.svg"
+  },
+  {
+    "revision": "3d4173a74fd0d242b03e39de7f248978",
+    "url": "static/media/confirm.3d4173a7.svg"
+  },
+  {
+    "url": "vendors~main.035b21baaa543542001c.bundle.js"
+  },
+  {
+    "url": "vendors~main.035b21baaa543542001c.bundle.js.LICENSE.txt"
+  }
+]);

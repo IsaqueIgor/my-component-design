0aab8f267527f259d633dc67c2db6a851855fcec
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-07-31 11:09:04 -0300

    add: Add customization to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,8 @@
 import { addParameters } from '@storybook/react';
 import { addDecorator } from '@storybook/react';
 import { withContexts } from '@storybook/addon-contexts/react';
+import { withKnobs } from '@storybook/addon-knobs';
+import { withA11y } from '@storybook/addon-a11y';
 import { contexts } from './contexts';
 
 addParameters({
@@ -11,3 +13,5 @@ addParameters({
 });
 
 addDecorator(withContexts(contexts));
+addDecorator(withKnobs);
+addDecorator(withA11y);
